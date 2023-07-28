09b7fbda12cb2daf71ce6bc168f4f0ba20f60ff8
Paulo Lopes <lopespcmlf@gmail.com>
2023-07-28 12:41:26 +0000

    Matriz

--- src/main/java/com/example/AppArray.java
+++ src/main/java/com/example/AppArray.java
@@ -5,8 +5,13 @@ public class AppArray {
     public static void main(String[] args) {
         var  x=10;
         int testearray[]= {10,30,40};
+        double matriz[][]={{1,2},{2,2}};
+        matriz[0]=new double[1];
 
-        System.out.println(Arrays.toString(testearray));
+     //   System.out.println(Arrays.toString(testearray));
+        System.out.println(Arrays.toString(matriz[0]));
+        System.out.println(Arrays.toString(matriz[1]));
+
     }
     
 }
